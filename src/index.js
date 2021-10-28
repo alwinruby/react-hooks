02672b639adb5f37a860d39bf95247af1a2052fc
alwinruby <alwinsolanky@gmail.com>
2021-10-28 13:56:38 +0100

    controlled components with useState

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function App(){
-  const sound = useRef();
-  const color = useRef();
+  const [sound, setSound] = useState("");
+  const [color, setColor] = useState("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    const soundVal = sound.current.value;
-    const colorVal = color.current.value;
-    alert(`${soundVal} sounds like ${colorVal}`);
-    sound.current.value = "";
-    color.current.value = "";
+    alert(`${sound} sounds like ${color}`);
+    setSound("");
+    setColor("#000000");
   };
 
   return (
     <div>
       <form onSubmit={submit}>
-        <input ref={sound} type="text" placeholder="Sound..." />
-        <input ref={color} type="color"/>
+        <input 
+          value={sound} 
+          type="text" 
+          placeholder="Sound..." 
+          onChange={(e) => setSound(e.target.value)}
+        />
+        <input 
+          value={color} 
+          type="color"
+          onChange={(e) => setColor(e.target.value)}
+        />
         <button>ADD</button>
       </form>
     </div>
