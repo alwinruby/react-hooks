482e8b85fe12fa06974e119fb681c7542ec74af5
alwinruby <alwinsolanky@gmail.com>
2021-10-28 07:46:55 +0100

    Complex state with useReducer

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function App(){
-  const [data, setData] = useState([]);
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber , 
+    0 
+  ); 
 
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-    .then(response => response.json())
-    .then(setData);
-  }, []);
-
-  if(data){
-    return(
-      <div>
-        <ul>
-          {data.map((user) => (
-            <li key={user.id}>{user.login}</li>
-          ))}
-        </ul>
-        <button onClick={() => setData([])}>Remove Data</button>
-      </div>
-    )
-  }
-
-  return <p>No Users</p>;
+  return (
+  <h1 onClick={ () => setNumber(1) }>{number}</h1>
+  );
 }
 
 
