7e400e159486fc20f1bc2098d016f20c5667359e
alwinruby <alwinsolanky@gmail.com>
2021-10-28 12:35:02 +0100

    form inputs with useRef

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const initialState = {
+  message: "Hi!"
+};
+
+function reducer(state, action)  {
+  switch(action.type){
+    case "yell":
+      return {
+        message: `HEY I JUST SAID ${state.message}`
+      };
+    case "whisper":
+      return {
+        message: `excuse me, I just said  ${state.message}`
+      };
+  }
+}
+
 function App(){
-  const [checked, toggle] = useReducer(
-    (checked) => !checked , 
-    false
+  const [state, dispatch] = useReducer(
+    reducer, 
+    initialState
   ); 
 
   return (
     <div>
-      <input 
-        type="checkbox" 
-        value={checked} 
-        onChange={ toggle } 
-      />
-      <p>{ checked ? "checked" : "not checked" }</p>
+      <p>Message : {state.message}</p>
+      <button onClick={() => dispatch({ type: "yell"}) }>YELL</button>
+      <button onClick={() => dispatch({ type: "whisper"}) }>whisper</button>
     </div>
   );
 }
